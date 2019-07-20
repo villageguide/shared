4198f16072b82b3e1477f9d7c11be17f04d59fb0
Ishan Jayamanne <ishan@silverstripe.com>
2019-07-20 23:45:11 +1200

    update

--- src/Models/Manager.php
+++ src/Models/Manager.php
@@ -29,12 +29,23 @@ class Manager extends Model
         return $this->belongsTo(Village::class);
     }
 
-
     /**
      * @return HasOne
      */
     public function photo()
     {
         return $this->hasOne(File::class, 'id', 'photo_id');
+    }
+
+    /**
+     * @return string
+     */
+    public function fullNameInitial()
+    {
+        $nameParts = explode(' ', $this->name);
+        $nameFirst = $nameParts[0];
+        $namLast = $nameParts[count($nameParts) - 1];
+
+        return sprintf('%s%s', substr($nameFirst, 0, 1), substr($namLast, 0, 1));
     }
 }

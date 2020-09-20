3b4f3ad16a689923fd398dbe4a942c13dc7253c8
Ishan Jayamanne <ishanjmails@gmail.com>
2020-09-20 10:25:19 +0000

    update

---
+++ src/Models/Advert.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Advert extends Model
+{
+    protected $dates = ['start_date','end_date' ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'type',
+        'region',
+        'start_date',
+        'end_date',
+        'file_id',
+    ];
+
+    /**
+     * @return File|HasOne
+     */
+    public function image()
+    {
+        return $this->hasOne(File::class, 'id', 'file_id');
+    }
+}

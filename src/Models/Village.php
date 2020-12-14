549cce94a7ee26775cfbf0500ed242d15726ba3d
Ishan Jayamanne <ishan@silverstripe.com>
2020-12-14 23:21:19 +1300

    update village virtual tour

--- src/Models/Village.php
+++ src/Models/Village.php
@@ -552,4 +552,14 @@ class Village extends Model
     {
         return $this->hasOne(VillagePremium::class);
     }
+
+    /**
+     * Get the virtual tours associated with the care home.
+     *
+     * @return HasMany
+     */
+    public function virtualTours()
+    {
+        return $this->hasMany(VirtualTour::class);
+    }
 }
